f9d74942f71f495a2f4b93b322b16f96a0c4fd1b
yangdejian-PC.CO <553849374@qq.com>
2016-05-18 14:49:04 +0800

    一次提交2个文件

--- LICENSE.md
+++ LICENSE.md
@@ -1,1 +1,6 @@
 asdfasdfasdf
+
+
++sdfsdfsdfsdfsdf
+
+ĞÂµÄÄÚÈİ
---
+++ mylua.lua
@@ -0,0 +1,11 @@
+
+require 'sys'
+require 'custom.common.wclient'
+require 'custom.common.xhtml'
+
+
+
+function main()
+
+	login()
+end
